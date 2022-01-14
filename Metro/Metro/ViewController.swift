84621528d1698c1e7a362a29d7ebfb1b78c2b744
egorzainullin <egor.zainullin@outlook.com>
2022-01-14 14:58:19 +0300

    -added events and tested

--- Metro/Metro/ViewController.swift
+++ Metro/Metro/ViewController.swift
@@ -11,10 +11,13 @@ class ViewController: UIViewController, UIScrollViewDelegate {
     
     @IBOutlet weak var mapScrollView: CustomScrollView!
     
+    @IBOutlet weak var contentView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         initMap()
+        registerClicksOnStationsAndLabels()
     }
 
     private func initMap() {
@@ -22,9 +25,28 @@ class ViewController: UIViewController, UIScrollViewDelegate {
     }
 
     private func registerClicksOnStationsAndLabels() {
-        let stations = view.subviews.compactMap{ $0 as? StationCircleView }
-        let labels = view.subviews.compactMap{ $0 as? NameLabel }
-        // TODO: add events
+        let stations = contentView.subviews.compactMap{ $0 as? StationCircleView }
+        let labels = contentView.subviews.compactMap{ $0 as? NameLabel }
+        for station in stations {
+            station.delegate = self
+        }
+        for label in labels {
+            label.delegate = self
+        }
     }
+}
+
+extension ViewController: StationCircleViewDelegate {
+    func stationClicked(_ sender: StationCircleView, id: String) {
+        // TODO: handle this event
+    }
+}
+
+extension ViewController: NameLabelDelegate {
+    func labelClicked(_ sender: NameLabel, id: String) {
+        // TODO: handle this event
+    }
+    
+    
 }
 
